4e5579c28a85c049df197a46b6564c4f10b68ef9
Takatoshi Soeda <toshi.scp11@gmail.com>
2025-06-29 18:12:32 -0700

    source

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,13 +1,17 @@
 #!/usr/bin/bash
 source /opt/ros/melodic/setup.bash
 
+# Add ROS and workspace setup to bashrc
+echo "source /opt/ros/melodic/setup.bash" >> ~/.bashrc
+echo "source /opt/barracuda-sonar/catkin_ws/devel/setup.bash" >> ~/.bashrc
+
 # Build & Source catkin_ws
 cd /opt/barracuda-sonar/catkin_ws
 catkin clean
 catkin build
 source devel/setup.bash
 
-roslaunch ping360_sonar example.launch || true
+roslaunch ping360_sonar example.launch
 
 # Keep container running if roslaunch fails or exits
 # while true; do sleep 1000; done
